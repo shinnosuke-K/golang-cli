99148f7b4b7763045f5bf28bf6ecb33d708552ce
shinnosuke-K <shin.050195@gmail.com>
2019-05-05 16:28:27 +0900

    section: 4.2.1.1

--- sprinkle/main.go
+++ sprinkle/main.go
@@ -24,11 +24,31 @@ var transforms = []string{
 	"lets" + otherWord,
 }
 
+func readFromFile(filePath string) []string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("error")
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	lines := make([]string, 0)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "File %s scan error: %v\n", filePath, err)
+	}
+	return lines
+}
+
 func main() {
+	lines := readFromFile("text")
 	rand.Seed(time.Now().UTC().UnixNano())
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
-		t := transforms[rand.Intn(len(transforms))]
+		t := lines[rand.Intn(len(lines))]
 		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
 	}
 }
